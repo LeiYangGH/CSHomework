190da20ac048384d98e8652cc75fd95048d11306
Lei Yang <675686066@qq.com>
2016-12-22 15:38:47 +0800

    first and last date

--- MonkeyKing/MonkeyKing/Program.cs
+++ MonkeyKing/MonkeyKing/Program.cs
@@ -8,15 +8,30 @@ namespace MonkeyKing
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+        //[STAThread]
+        //static void Main()
+        //{
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new frmPyramid());
+        //}
+
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmPyramid());
+            CalcFirstAndLastDay();
+            Console.ReadLine();
         }
+
+        static void CalcFirstAndLastDay()
+        {
+            Console.WriteLine("注：你可以通过修改电脑年份，运行后可以看到不同结果");
+            DateTime now = DateTime.Now;
+            int year = now.Year;
+            DateTime firstDay = new DateTime(year, 1, 1).Date;
+            DateTime lastDay = new DateTime(year, 12, 31).Date;
+            Console.WriteLine("今年第一天" + firstDay.ToShortDateString());
+            Console.WriteLine("今年最后一天" + lastDay.ToShortDateString());
+        }
+
     }
 }
