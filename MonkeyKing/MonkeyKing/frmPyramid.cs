4642ef10e169d4926df00354be96ed74401fc8fc
Lei Yang <675686066@qq.com>
2016-12-22 14:47:07 +0800

    basic ok

--- MonkeyKing/MonkeyKing/frmPyramid.cs
+++ MonkeyKing/MonkeyKing/frmPyramid.cs
@@ -27,11 +27,47 @@ namespace MonkeyKing
 
         }
 
+        private bool CheckInput(out int delta, out int layer)
+        {
+            delta = (int)this.numericUpDown1.Value;
+            layer = (int)this.numericUpDown2.Value;
+            if (delta % 2 != 0)
+            {
+                MessageBox.Show("每行多的应该是偶数！");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private void DrawPyramid(Graphics g, int delta, int layer)
+        {
+            for (int lay = 0; lay < layer; lay++)
+            {
+                int dx = 9;
+                int xm = lay * delta / 2 * dx;
+                int y = lay * 10;
+                for (int x = 0; x < xm; x += dx)
+                {
+                    DrawAt(g, x, y);
+                    DrawAt(g, -x, y);
+                }
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
+
             Graphics g = this.pictureBox1.CreateGraphics();
-            g.TranslateTransform(this.pictureBox1.Width / 2, 10);
-            DrawAt(g, -10, -10);
+            g.TranslateTransform(this.pictureBox1.Width / 2, 0);
+
+            int delta;
+            int layer;
+            g.Clear(SystemColors.Control);
+            if (this.CheckInput(out delta, out layer))
+            {
+                this.DrawPyramid(g, delta, layer);
+            }
         }
 
         private void DrawAt(Graphics g, int x, int y)
