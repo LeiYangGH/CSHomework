a757d84608b604cc0f69efda10d0572214186345
Lei Yang <675686066@qq.com>
2016-12-23 09:43:12 +0800

    Program1 prime 1000

---
+++ MonkeyKing/CSchenyi96225/CSchenyi96225/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSchenyi96225
+{
+    class Program
+    {
+        static void Main0(string[] args)
+        {
+        }
+    }
+}
---
+++ MonkeyKing/CSchenyi96225/CSchenyi96225/Program1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSchenyi96225
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int num = 1000;
+            while (true)
+            {
+                if (IsPrime(num--))
+                {
+                    Console.WriteLine("1000以内的最大素数:{0}", num);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int num)
+        {
+            if (num == 1) return false;
+            if (num == 2) return true;
+            int b = (int)Math.Floor(Math.Sqrt(num));
+            for (int i = 2; i <= b; ++i)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
