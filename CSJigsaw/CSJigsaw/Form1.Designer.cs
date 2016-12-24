0d0154a134aa81f07807bc964476c32839bda4dc
LeiYangGH <675686066@qq.com>
2016-12-24 10:52:44 +0800

    only show 15 and can shuffle

--- CSJigsaw/CSJigsaw/Form1.Designer.cs
+++ CSJigsaw/CSJigsaw/Form1.Designer.cs
@@ -28,13 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnShuffle = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.Location = new System.Drawing.Point(810, 486);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(105, 44);
+            this.btnShuffle.TabIndex = 0;
+            this.btnShuffle.Text = "洗牌";
+            this.btnShuffle.UseVisualStyleBackColor = true;
+            this.btnShuffle.Click += new System.EventHandler(this.btnShuffle_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(927, 605);
+            this.Controls.Add(this.btnShuffle);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -44,6 +56,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnShuffle;
     }
 }
 
--- CSJigsaw/CSJigsaw/Form1.cs
+++ CSJigsaw/CSJigsaw/Form1.cs
@@ -13,18 +13,20 @@ namespace CSJigsaw
     public partial class Form1 : Form
     {
         public int level = 4;
+        public int btnsCount;
         Keys[] keys = { Keys.Left, Keys.Right, Keys.Up, Keys.Down };
         int xEmpty, yEmpty, iCounter;
-
+        private List<Button> lstButtons;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void AddButtons()
         {
             int cnt = 0;
             Size btnSize = new Size(50, 50);
+            this.lstButtons = new List<Button>();
             for (int j = 0; j < level; j++)
             {
                 for (int i = 0; i < level; i++)
@@ -35,9 +37,35 @@ namespace CSJigsaw
                     //tile.MouseLeftButtonDown += TileOnMouseLeftButtonDown; ;
                     btn.Left = 10 + i * btnSize.Width;
                     btn.Top = 10 + j * btnSize.Height;
+                    this.lstButtons.Add(btn);
                     this.Controls.Add(btn);
                 }
             }
+            Button last = this.lstButtons.Last();
+            this.Controls.Remove(last);
+            this.lstButtons.Remove(last);
+            this.btnsCount = this.level * this.level - 1;
+        }
+
+        private void Shuffle()
+        {
+            Random r = new Random();
+            var randIds = Enumerable.Range(1, this.btnsCount)
+                .OrderBy(x => r.Next()).ToArray();
+            for (int i = 0; i < btnsCount; i++)
+            {
+                this.lstButtons[i].Text = randIds[i].ToString();
+            }
+        }
+
+        private void btnShuffle_Click(object sender, EventArgs e)
+        {
+            this.Shuffle();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AddButtons();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
