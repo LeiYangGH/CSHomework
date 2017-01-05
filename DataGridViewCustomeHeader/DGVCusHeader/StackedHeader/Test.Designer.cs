7fa6760e0afe68a28cd7312f906e5f3e0bcce47d
LeiYangGH <675686066@qq.com>
2017-01-05 16:52:14 +0800

    StackedHeader

---
+++ DataGridViewCustomeHeader/DGVCusHeader/StackedHeader/Test.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace StackedHeader
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.e = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.e,
+            this.Column2,
+            this.ee});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(588, 323);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnAdd.Location = new System.Drawing.Point(0, 341);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(612, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Columns";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Parent.Child 1.Input 1";
+            this.Column1.Name = "Column1";
+            // 
+            // e
+            // 
+            this.e.HeaderText = "Parent.Child 1.Input 2";
+            this.e.Name = "e";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Parent.Input 3";
+            this.Column2.Name = "Column2";
+            // 
+            // ee
+            // 
+            this.ee.HeaderText = "Parent.Input 4";
+            this.ee.Name = "ee";
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 364);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Test";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn e;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ee;
+    }
+}
+
---
+++ DataGridViewCustomeHeader/DGVCusHeader/StackedHeader/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StackedHeader
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+
+            StackedHeaderDecorator objREnderer = new StackedHeaderDecorator(dataGridView1);
+            //dataGridView1.Columns[0].Visible = false;
+            //dataGridView1.Columns[2].Visible = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                int columnCount = dataGridView1.Columns.Count;
+                dataGridView1.Columns.Add("Column" + dataGridView1.Columns.Count,
+                                          "Parent[" + columnCount + "].Child 1.Child 2.Input Name");
+                dataGridView1.Columns.Add("Column" + dataGridView1.Columns.Count ,
+                                          "Parent[" + columnCount + "].Child 1.Child 2.Input Name 2");
+                dataGridView1.Columns.Add("Column" + dataGridView1.Columns.Count,
+                                          "Parent[" + columnCount + "].Child 1.Child 3.Input Name 3");
+                dataGridView1.Columns.Add("Column" + dataGridView1.Columns.Count ,
+                                          "Parent[" + columnCount + "].Child 1.Input Name 4");
+                dataGridView1.Columns.Add("Column" + dataGridView1.Columns.Count ,
+                                          "Parent[" + columnCount + "].Input Name 5");
+            }
+        }
+    }
+}
