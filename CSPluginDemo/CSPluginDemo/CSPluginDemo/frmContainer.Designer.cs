7ec0bc9a35dc5b91fa8cc28874e264978159ee6b
LeiYangGH <675686066@qq.com>
2017-01-06 18:17:58 +0800

    can get cut plugin

--- CSPluginDemo/CSPluginDemo/CSPluginDemo/frmContainer.Designer.cs
+++ CSPluginDemo/CSPluginDemo/CSPluginDemo/frmContainer.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.容器菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.刷新插件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,16 +48,34 @@
             // 
             // 容器菜单ToolStripMenuItem
             // 
+            this.容器菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.刷新插件ToolStripMenuItem});
             this.容器菜单ToolStripMenuItem.Name = "容器菜单ToolStripMenuItem";
             this.容器菜单ToolStripMenuItem.Size = new System.Drawing.Size(100, 29);
             this.容器菜单ToolStripMenuItem.Text = "容器菜单";
             this.容器菜单ToolStripMenuItem.Click += new System.EventHandler(this.容器菜单ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(743, 485);
+            this.textBox1.TabIndex = 1;
+            // 
+            // 刷新插件ToolStripMenuItem
+            // 
+            this.刷新插件ToolStripMenuItem.Name = "刷新插件ToolStripMenuItem";
+            this.刷新插件ToolStripMenuItem.Size = new System.Drawing.Size(211, 30);
+            this.刷新插件ToolStripMenuItem.Text = "刷新插件";
+            this.刷新插件ToolStripMenuItem.Click += new System.EventHandler(this.刷新插件ToolStripMenuItem_Click);
             // 
             // frmContainer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 582);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmContainer";
@@ -71,6 +91,8 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem 容器菜单ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 刷新插件ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CSPluginDemo/CSPluginDemo/CSPluginDemo/frmContainer.cs
+++ CSPluginDemo/CSPluginDemo/CSPluginDemo/frmContainer.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using IPlugin;
 namespace CSPluginDemo
 {
     public partial class frmContainer : Form
     {
+        private List<Type> lstTypes;
         public frmContainer()
         {
             InitializeComponent();
@@ -19,7 +22,45 @@ namespace CSPluginDemo
 
         private void 容器菜单ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("容器自带菜单而已");
+            //MessageBox.Show("容器自带菜单而已");
+        }
+
+        private List<Type> GetExeTypes()
+        {
+            var lst = new List<Type>();
+            foreach (string fileName in Directory.GetFiles(Environment.CurrentDirectory, "*.exe"))
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(fileName);
+                    var t = assembly.GetTypes().FirstOrDefault(x => typeof(IMenuPlugin).IsAssignableFrom(x));
+                    if (t != null)
+                        lst.Add(t);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            return lst;
+        }
+
+        private void 刷新插件ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.lstTypes = this.GetExeTypes();
+            foreach (var t in this.lstTypes)
+            {
+                IMenuPlugin instance = (IMenuPlugin)Activator.CreateInstance(t);
+                foreach (var kv in instance.GetMenus())
+                {
+                    ToolStripMenuItem item = new ToolStripMenuItem(kv.Key);
+                    item.Click += (s, ee) =>
+                      {
+                          kv.Value.Invoke(this.textBox1);
+                      };
+                    this.menuStrip1.Items.Add(item);
+                }
+            }
         }
     }
 }
