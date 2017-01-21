a985940bcd116ed47aa0e86f0faebcca61bbd9e2
LeiYangGH <675686066@qq.com>
2017-01-22 07:47:38 +0800

    can take screen, but seems only part

---
+++ CSCapture/CSCapture/CSCapture/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CSCapture
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDesktop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDesktop
+            // 
+            this.btnDesktop.Location = new System.Drawing.Point(205, 207);
+            this.btnDesktop.Name = "btnDesktop";
+            this.btnDesktop.Size = new System.Drawing.Size(133, 39);
+            this.btnDesktop.TabIndex = 0;
+            this.btnDesktop.Text = "全屏截图";
+            this.btnDesktop.UseVisualStyleBackColor = true;
+            this.btnDesktop.Click += new System.EventHandler(this.btnDesktop_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 272);
+            this.Controls.Add(this.btnDesktop);
+            this.Name = "frmMain";
+            this.Text = "截图示例";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDesktop;
+    }
+}
+
---
+++ CSCapture/CSCapture/CSCapture/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSCapture
+{
+    public partial class frmMain : Form
+    {
+        string dir = "Captures";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateDirectory()
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+        }
+
+        private void CaptureEntireScreen(string fileName)
+        {
+            var bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                           Screen.PrimaryScreen.Bounds.Height,
+                                           PixelFormat.Format32bppArgb);
+
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                        Screen.PrimaryScreen.Bounds.Y,
+                                        0,
+                                        0,
+                                        Screen.PrimaryScreen.Bounds.Size,
+                                        CopyPixelOperation.SourceCopy);
+
+            bmpScreenshot.Save(fileName, ImageFormat.Png);
+        }
+        private void btnDesktop_Click(object sender, EventArgs e)
+        {
+            string timeString = DateTime.Now.ToString("HHmmss");
+            string fileName = Path.Combine(Environment.CurrentDirectory, dir, timeString);
+            this.CaptureEntireScreen(fileName + ".png");
+            MessageBox.Show(fileName);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.CreateDirectory();
+        }
+    }
+}
