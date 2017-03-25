39063d322fc7852b80f66b929633ed6bf2dc661c
LeiYangGH <675686066@qq.com>
2017-03-25 18:16:11 +0800

    add sql and entity

--- CSKnowledge/CSKnowledge/CSKnowledge/Form1.Designer.cs
+++ CSKnowledge/CSKnowledge/CSKnowledge/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSKnowledge
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFind = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(131, 57);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(116, 37);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "添加新知识";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(131, 156);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(116, 37);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "查阅旧知识";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 436);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(396, 271);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "frmMain";
+            this.Text = "我的知识库";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFind;
     }
 }
 
