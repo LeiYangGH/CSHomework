e23f912c36cf192c35b78c1c47378e7672afa26d
LeiYangGH <675686066@qq.com>
2017-03-29 13:56:58 +0800

    ToDecrypt

---
+++ CSDES/CSDEC/CSDEC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDEC
+{
+    class Program
+    {
+        //ToDecrypt(“4z8BwworyHQ=”, "IccoWeb!@#")  输出是50
+        private static string ToDecrypt(string strText, string sDecrKey)
+        {
+            byte[] rgbKey = null;
+            byte[] rgbIV = new byte[] { 18, 52, 86, 120, 144, 171, 205, 239 };
+            byte[] buffer = new byte[strText.Length];
+            try
+            {
+                rgbKey = Encoding.UTF8.GetBytes(sDecrKey.Substring(0, 8));
+                DESCryptoServiceProvider provider = new DESCryptoServiceProvider();
+                buffer = Convert.FromBase64String(strText);
+                MemoryStream stream = new MemoryStream();
+                CryptoStream stream2 = new CryptoStream(stream, provider.CreateDecryptor(rgbKey, rgbIV), CryptoStreamMode.Write);
+                stream2.Write(buffer, 0, buffer.Length);
+                stream2.FlushFinalBlock();
+                Encoding encoding = new UTF8Encoding();
+                return encoding.GetString(stream.ToArray());
+            }
+            catch (Exception exception)
+            {
+                return exception.Message.ToString();
+            }
+        }
+        static void Main(string[] args)
+        {
+            string de = ToDecrypt("4z8BwworyHQ =", "IccoWeb!@#");
+            Console.WriteLine(de);
+            Console.ReadLine();
+        }
+    }
+}
