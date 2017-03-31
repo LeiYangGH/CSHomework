7c1a5b72b3ed8697afc05afd307bf8105cab488e
LeiYangGH <675686066@qq.com>
2017-03-31 21:10:17 +0800

    ButtonEventSeries

---
+++ ButtonEventSeries/ButtonEventSeries/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonEventSeries
+{
+    public partial class Form1 : Form
+    {
+        Action[] acts;
+        public Form1()
+        {
+            InitializeComponent();
+            this.acts = new Action[]
+            {
+                ()=> this.button1_Click(null,null),
+                ()=> this.button2_Click(null,null),
+                ()=> this.button3_Click(null,null),
+                ()=> this.button4_Click(null,null),
+            };
+        }
+
+        private void AddText(string txt)
+        {
+            this.textBox1.Text += $"{txt}\r\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.AddText("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.AddText("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.AddText("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.AddText("4");
+        }
+
+        bool Done;
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            if (this.Done)
+                this.timer1.Stop();
+            else
+            {
+                this.Done = true;
+                this.timer1.Start();
+            }
+        }
+
+        private int cnt = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.acts[++this.cnt % this.acts.Length].Invoke();
+        }
+    }
+}
---
+++ ListHighlight/ListHighlight/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListHighlight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelecteItemByIndex(int index1)
+        {
+            if (index1 > this.listBox1.Items.Count)
+                return;
+            this.listBox1.SelectedIndex = index1 - 1;
+        }
+
+        int index1 = 1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            //for (int i = 1; i <= 10; i++)
+            //    this.listBox1.Items.Add(i);
+            this.listBox1.Items.Add("Cap xx");
+            this.listBox1.Items.Add("3.3V");
+            this.listBox1.Items.Add("OlEDev +1V");
+            this.SelecteItemByIndex(index1);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            this.index1++;
+            this.SelecteItemByIndex(index1);
+        }
+    }
+}
