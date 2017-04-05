38a010f11a68419ef69c242e14c35ab33e3c123c
LeiYangGH <675686066@qq.com>
2017-04-05 21:17:08 +0800

    add CSBusHardwareTest

--- ButtonEventSeries/ButtonEventSeries/Form1.cs
+++ ButtonEventSeries/ButtonEventSeries/Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Threading;
 namespace ButtonEventSeries
 {
     public partial class Form1 : Form
@@ -27,7 +20,7 @@ namespace ButtonEventSeries
 
         private void AddText(string txt)
         {
-            this.textBox1.Text += $"{txt}\r\n";
+            this.textBox1.Text += txt + "\r\n";
         }
 
         private void button1_Click(object sender, EventArgs e)
