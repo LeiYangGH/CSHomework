e564b488442f002cfecfa7fcae2ef82da095f3a9
LeiYangGH <675686066@qq.com>
2017-04-21 16:54:24 +0800

    GetTxt4Lines

---
+++ CSNumStaitcs/CSNumStaitcs/NumGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSNumStaitcs
+{
+    public class NumGenerator
+    {
+        private int[] nums;
+        public NumGenerator(int[] nums)
+        {
+            this.nums = nums;
+        }
+
+        public int[,] Get34Seeds()
+        {
+            return new int[4, 3]
+                   {
+                    { nums[0],nums[2],nums[5]},
+                    { nums[1],nums[3],nums[5]},
+                    { nums[0],nums[3],nums[4]},
+                    { nums[1],nums[2],nums[4]},
+                   };
+        }
+
+        public int[] Result2(int a, int b)
+        {
+            int r0 = (a + b) % 10;
+            int r1 = (a >= b ? a : a + 10) - b;
+            return new int[] { r0, r1 };
+        }
+
+
+        public string GetTxt1Line(string description, int a, int b, int c)
+        {
+            int[] rab = Result2(a, b);
+            int[] rbc = Result2(b, c);
+            int[] rca = Result2(c, a);
+            return $"{description}  {a}{b}{c}-{a}{b}-{b}{c}-{c}{a}={rab[0]}{rab[1]}-{rbc[0]}{rbc[1]}-{rca[0]}{rca[1]}\r\n";
+        }
+
+        public string GetTxt4Lines()
+        {
+            int[,] S34 = this.Get34Seeds();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(GetTxt1Line("奇奇=偶", S34[0,0], S34[0, 1], S34[0, 2]));
+            sb.Append(GetTxt1Line("偶偶=偶", S34[1,0], S34[1, 1], S34[1, 2]));
+            sb.Append(GetTxt1Line("奇偶=奇", S34[2,0], S34[2, 1], S34[2, 2]));
+            sb.Append(GetTxt1Line("偶奇=奇", S34[3,0], S34[3, 1], S34[3, 2]));
+            return sb.ToString();
+        }
+    }
+}
--- CSNumStaitcs/CSNumStaitcs/Program.cs
+++ CSNumStaitcs/CSNumStaitcs/Program.cs
@@ -7,6 +7,7 @@ namespace CSNumStaitcs
 {
     static class Program
     {
+#if win
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -17,5 +18,16 @@ namespace CSNumStaitcs
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+#else
+        [STAThread]
+        static void Main()
+        {
+            NumGenerator gen = new NumGenerator(new int[] { 6, 5, 4, 3, 7, 6 });
+            object o = gen.Get34Seeds();
+            Console.WriteLine(o);
+            Console.WriteLine(gen.GetTxt4Lines());
+            Console.ReadLine();
+        }
+#endif
     }
 }
