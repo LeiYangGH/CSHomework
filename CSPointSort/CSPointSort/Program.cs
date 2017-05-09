6025779f250f890a5593db162a1682f486f7dd19
LeiYangGH <675686066@qq.com>
2017-05-09 16:14:23 +0800

    ReadListFromDat

--- CSPointSort/CSPointSort/Program.cs
+++ CSPointSort/CSPointSort/Program.cs
@@ -18,11 +18,24 @@ namespace CSPointSort
                 .Select(x => x.Trim()).ToList();
         }
 
+        static List<string[]> ReadListFromDat()
+        {
+            return File.ReadAllLines(datFileName)
+                .Select(x =>
+                (x.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(y => y.Trim())).ToArray()).ToList();
+        }
+
         static void Main(string[] args)
         {
             foreach (string line in ReadListFromA0())
             {
                 Console.WriteLine(line);
+            }
+
+            foreach (string[] linewords in ReadListFromDat())
+            {
+                Console.WriteLine(string.Join(" ", linewords));
             }
             Console.ReadLine();
         }
