605c070376bfe6a110635373aa1d1299ba459d95
LeiYangGH <675686066@qq.com>
2017-05-09 16:03:44 +0800

    ReadListFromA0

--- CSPointSort/CSPointSort/Program.cs
+++ CSPointSort/CSPointSort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,22 @@ namespace CSPointSort
 {
     class Program
     {
+        const string a0FileName = "A0.txt";
+        const string datFileName = "Elist.dat";
+
+        static List<string> ReadListFromA0()
+        {
+            return File.ReadAllLines(a0FileName)
+                .Select(x => x.Trim()).ToList();
+        }
+
         static void Main(string[] args)
         {
+            foreach (string line in ReadListFromA0())
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
         }
     }
 }
