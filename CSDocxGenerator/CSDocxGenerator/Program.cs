9c20b9cff3f92d4faa92063907f35f8fd2df2945
LeiYangGH <675686066@qq.com>
2017-05-25 18:54:41 +0800

    can read word

---
+++ CSDocxGenerator/CSDocxGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDocxGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open a doc file.
+            string fn = Path.Combine(Environment.CurrentDirectory, "w.docx");
+            Application application = new Application();
+            Document document = application.Documents.Open(fn);
+
+            // Loop through all words in the document.
+            int count = document.Words.Count;
+            for (int i = 1; i <= count; i++)
+            {
+                // Write the word.
+                string text = document.Words[i].Text;
+                Console.WriteLine("Word {0} = {1}", i, text);
+            }
+            // Close word.
+            application.Quit();
+            Console.ReadLine();
+        }
+    }
+}
