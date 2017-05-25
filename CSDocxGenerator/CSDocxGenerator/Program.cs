383839ec1f0240f30146a782c3990102f0d11ccd
LeiYangGH <675686066@qq.com>
2017-05-25 19:10:53 +0800

    can generate doc

--- CSDocxGenerator/CSDocxGenerator/Program.cs
+++ CSDocxGenerator/CSDocxGenerator/Program.cs
@@ -5,29 +5,57 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Word = Microsoft.Office.Interop.Word;
 namespace CSDocxGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Open a doc file.
-            string fn = Path.Combine(Environment.CurrentDirectory, "w.docx");
-            Application application = new Application();
-            Document document = application.Documents.Open(fn);
+            Console.WriteLine("请输入生成的序号从多少数字开始：");
+            int from = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入生成的序号从多少数字结束：");
+            int to = Convert.ToInt32(Console.ReadLine());
 
-            // Loop through all words in the document.
-            int count = document.Words.Count;
-            for (int i = 1; i <= count; i++)
+            for (int i = from; i <= to; i++)
             {
-                // Write the word.
-                string text = document.Words[i].Text;
-                Console.WriteLine("Word {0} = {1}", i, text);
+                CreateDocx(i);
+
             }
-            // Close word.
-            application.Quit();
+            Console.WriteLine("结束！");
             Console.ReadLine();
+        }
+
+        static void CreateDocx(int n)
+        {
+            string fn = Path.Combine(Environment.CurrentDirectory, $"{n}.docx");
+
+
+            object oMissing = System.Reflection.Missing.Value;
+            //object oEndOfDoc = "\\endofdoc"; /* \endofdoc is a predefined bookmark */
+
+            //Start Word and create a new document.
+            Word._Application oWord;
+            Word._Document oDoc;
+            oWord = new Word.Application();
+            //oWord.Visible = true;
+            oDoc = oWord.Documents.Add(ref oMissing, ref oMissing,
+            ref oMissing, ref oMissing);
+
+            //Insert a paragraph at the beginning of the document.
+            Word.Paragraph oPara1;
+            oPara1 = oDoc.Content.Paragraphs.Add(ref oMissing);
+            oPara1.Range.Text = $"2017年度部卫片{n}号";
+            oPara1.Range.Font.Bold = 1;
+            oPara1.Range.Font.Size = 36;
+            //oPara1.Format.SpaceAfter = 36;    //24 pt spacing after paragraph.
+            oPara1.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;    //24 pt spacing after paragraph.
+            oPara1.Range.InsertParagraphAfter();
+
+
+            oDoc.SaveAs2(fn);
+            oWord.Quit();
+            Console.WriteLine(fn);
         }
     }
 }
