150896f3c1cefbaf1c11a9d0e70a7cb457b7ab35
LeiYangGH <675686066@qq.com>
2017-05-26 18:40:35 +0800

    all in one docx, one num one page

--- CSDocxGenerator/CSDocxGenerator/Program.cs
+++ CSDocxGenerator/CSDocxGenerator/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Word = Microsoft.Office.Interop.Word;
@@ -16,42 +17,43 @@ namespace CSDocxGenerator
             int from = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("请输入生成的序号从多少数字结束：");
             int to = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = from; i <= to; i++)
-            {
-                CreateDocx(i);
-
-            }
+            CreateDocx(from, to);
+            //CreateDocx(1, 3);
             Console.WriteLine("结束！");
             Console.ReadLine();
         }
 
-        static void CreateDocx(int n)
+        static void CreateDocx(int from, int to)
         {
-            string fn = Path.Combine(Environment.CurrentDirectory, $"{n}.docx");
+            string fn = Path.Combine(Environment.CurrentDirectory, "test.docx");
 
 
             object oMissing = System.Reflection.Missing.Value;
-            //object oEndOfDoc = "\\endofdoc"; /* \endofdoc is a predefined bookmark */
 
-            //Start Word and create a new document.
             Word._Application oWord;
             Word._Document oDoc;
             oWord = new Word.Application();
-            //oWord.Visible = true;
+
             oDoc = oWord.Documents.Add(ref oMissing, ref oMissing,
             ref oMissing, ref oMissing);
-
-            //Insert a paragraph at the beginning of the document.
             Word.Paragraph oPara1;
             oPara1 = oDoc.Content.Paragraphs.Add(ref oMissing);
-            oPara1.Range.Text = $"2017年度部卫片{n}号";
+            oPara1.Range.Text = $"2017年度部卫片";
             oPara1.Range.Font.Bold = 1;
             oPara1.Range.Font.Size = 36;
-            //oPara1.Format.SpaceAfter = 36;    //24 pt spacing after paragraph.
-            oPara1.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;    //24 pt spacing after paragraph.
+            oPara1.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
             oPara1.Range.InsertParagraphAfter();
+            for (int i = from; i <= to; i++)
+            {
+                oDoc.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
 
+                oPara1 = oDoc.Content.Paragraphs.Add(ref oMissing);
+                oPara1.Range.Text = i.ToString();
+                oPara1.Range.Font.Bold = 1;
+                oPara1.Range.Font.Size = 36;
+                oPara1.Format.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                oPara1.Range.InsertParagraphAfter();
+            }
 
             oDoc.SaveAs2(fn);
             oWord.Quit();
