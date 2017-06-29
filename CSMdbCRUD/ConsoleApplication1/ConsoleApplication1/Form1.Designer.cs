893309996d36dfab5ab3097ea12f919111d6ec0c
LeiYangGH <675686066@qq.com>
2017-06-29 15:31:05 +0800

    CSMdbCRUD done

--- CSMdbCRUD/ConsoleApplication1/ConsoleApplication1/Form1.Designer.cs
+++ CSMdbCRUD/ConsoleApplication1/ConsoleApplication1/Form1.Designer.cs
@@ -28,27 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.peifang2DataSet = new ConsoleApplication1.peifang2DataSet();
+            this.peifang2BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.peifang2TableAdapter = new ConsoleApplication1.peifang2DataSetTableAdapters.peifang2TableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.骨料DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.粉料DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.水DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peifang2DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peifang2BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(-14, 39);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.骨料DataGridViewTextBoxColumn,
+            this.粉料DataGridViewTextBoxColumn,
+            this.水DataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.peifang2BindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(-21, 65);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(776, 185);
+            this.dataGridView1.Size = new System.Drawing.Size(1164, 308);
             this.dataGridView1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(600, 230);
+            this.button1.Location = new System.Drawing.Point(900, 383);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 38);
             this.button1.TabIndex = 3;
             this.button1.Text = "保存";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +75,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(292, 230);
+            this.button2.Location = new System.Drawing.Point(438, 383);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(112, 38);
             this.button2.TabIndex = 4;
             this.button2.Text = "添加行";
             this.button2.UseVisualStyleBackColor = true;
@@ -66,26 +86,69 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(417, 230);
+            this.button3.Location = new System.Drawing.Point(626, 383);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(112, 38);
             this.button3.TabIndex = 5;
             this.button3.Text = "删除行";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // peifang2DataSet
+            // 
+            this.peifang2DataSet.DataSetName = "peifang2DataSet";
+            this.peifang2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // peifang2BindingSource
+            // 
+            this.peifang2BindingSource.DataMember = "peifang2";
+            this.peifang2BindingSource.DataSource = this.peifang2DataSet;
+            // 
+            // peifang2TableAdapter
+            // 
+            this.peifang2TableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // 骨料DataGridViewTextBoxColumn
+            // 
+            this.骨料DataGridViewTextBoxColumn.DataPropertyName = "骨料";
+            this.骨料DataGridViewTextBoxColumn.HeaderText = "骨料";
+            this.骨料DataGridViewTextBoxColumn.Name = "骨料DataGridViewTextBoxColumn";
+            // 
+            // 粉料DataGridViewTextBoxColumn
+            // 
+            this.粉料DataGridViewTextBoxColumn.DataPropertyName = "粉料";
+            this.粉料DataGridViewTextBoxColumn.HeaderText = "粉料";
+            this.粉料DataGridViewTextBoxColumn.Name = "粉料DataGridViewTextBoxColumn";
+            // 
+            // 水DataGridViewTextBoxColumn
+            // 
+            this.水DataGridViewTextBoxColumn.DataPropertyName = "水";
+            this.水DataGridViewTextBoxColumn.HeaderText = "水";
+            this.水DataGridViewTextBoxColumn.Name = "水DataGridViewTextBoxColumn";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 262);
+            this.ClientSize = new System.Drawing.Size(1238, 437);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peifang2DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peifang2BindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -96,5 +159,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private peifang2DataSet peifang2DataSet;
+        private System.Windows.Forms.BindingSource peifang2BindingSource;
+        private peifang2DataSetTableAdapters.peifang2TableAdapter peifang2TableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 骨料DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 粉料DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 水DataGridViewTextBoxColumn;
     }
 }
--- CSMdbCRUD/ConsoleApplication1/ConsoleApplication1/Form1.cs
+++ CSMdbCRUD/ConsoleApplication1/ConsoleApplication1/Form1.cs
@@ -21,130 +21,35 @@ namespace ConsoleApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=D:\\peifang2.mdb"); //Jet OLEDB:Database Password=
-            OleDbCommand cmd = conn.CreateCommand();
+            // TODO: 这行代码将数据加载到表“peifang2DataSet.peifang2”中。您可以根据需要移动或删除它。
+            this.peifang2TableAdapter.Fill(this.peifang2DataSet.peifang2);
 
-            cmd.CommandText = "select * from peifang2";
-            conn.Open();
-            OleDbDataReader dr = cmd.ExecuteReader();
-            DataTable dt = new DataTable();
-            if (dr.HasRows)
-            {
-                for (int i = 0; i < dr.FieldCount; i++)
-                {
-                    dt.Columns.Add(dr.GetName(i));
-                }
-                dt.Rows.Clear();
-            }
-            while (dr.Read())
-            {
-                DataRow row = dt.NewRow();
-                for (int i = 0; i < dr.FieldCount; i++)
-                {
-                    row[i] = dr[i];
-                }
-                dt.Rows.Add(row);
-            }
-            cmd.Dispose();
-            // conn.Close();
-            dataGridView1.DataSource = dt;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //弹出确认消息框
-            DialogResult result = MessageBox.Show("确定要将修改保存到数据库吗？",
-                "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
-            if (result == DialogResult.OK)
+            try
             {
-                /*       try
-
-                       {
-
-                           SqlConnection scon = new SqlConnection("数据库连接字");
-
-                           scon.Open();
-
-                           SqlCommand scmd;
-
-                           for (int i = 0; i < dataGridView1.Rows.Count; i++)
-
-                           {
-
-                               string id = dataGridView1.Rows[i].Cells["id"].EditedFormattedValue.ToString();
-
-                               string name = dataGridView1.Rows[i].Cells["name"].EditedFormattedValue.ToString();
-
-                               string age = dataGridView1.Rows[i].Cells["age"].EditedFormattedValue.ToString();
-
-                               string address = dataGridView1.Rows[i].Cells["address"].EditedFormattedValue.ToString();
-
-                               string scmdStr = "update studentInfo set name='" + name + "',age='" + age + "',address='" + address + "' where id ='" + id + "'";
-
-                               scmd = new SqlCommand(scmdStr, scon);
-
-                               scmd.ExecuteNonQuery();
-
-                           }
-
-                           scon.Close();
-
-                       }
-
-                       catch (Exception ex)
-
-                       {
-
-                           MessageBox.Show(ex.ToString());
-
-                       }
-                       */
-
-
-
-
-
-                for (int i = 0; i < dataGridView1.Rows.Count; i++)
-                {
-
-
-                    string 骨料 = dataGridView1.Rows[i].Cells["骨料"].Value.ToString();
-                    string 粉料 = dataGridView1.Rows[i].Cells["粉料"].Value.ToString();
-                    string guliao3 = dataGridView1.Rows[i].Cells["水"].Value.ToString();
-                    //   string fenliao = dataGridView1.Rows[1].Cells[2].Value.ToString();
-                    //    string shui = dataGridView1.Rows[1].Cells[3].Value.ToString();
-
-
-                    string inst = "INSERT INTO peifang2 values('" + 骨料 + "','" + 粉料 + "','" + guliao3 + "')";
-
-                    SqlCommand cmd = new SqlCommand();
-                  //  cmd.Connection = conn;
-
-                 //   OleDbCommand cmd = new OleDbCommand(inst, conn);
-
-                    cmd.ExecuteNonQuery();
-
-                    //     string scmdStr = "update studentInfo set 骨料='" + 骨料 + "',粉料='" + 粉料 + "'";
-                    //     OleDbCommand cmd = new OleDbCommand(scmdStr, conn);
-                    //         cmd.ExecuteNonQuery();
-
-                    // cmd = new SqlCommand(scmdStr, scon);
-
-                    // scmd.ExecuteNonQuery();
-
-
-                }
-
+                this.peifang2TableAdapter.Update(this.peifang2DataSet.peifang2);
+                MessageBox.Show("ok");
 
             }
-            //conn.Close();
-            MessageBox.Show("保存成功", "信息提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+           
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            this.peifang2DataSet.peifang2.Rows.Add(this.peifang2DataSet.peifang2.NewRow());
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.peifang2DataSet.peifang2.Rows[this.dataGridView1.CurrentCell.RowIndex].Delete();
         }
     }
 }
