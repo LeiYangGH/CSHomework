55c5fcdf99718ed195d81d86a5647cbccaa4f4c6
LeiYangGH <675686066@qq.com>
2017-08-09 17:40:43 +0800

    VBWrite3Files

---
+++ VBWrite3Files/VBWrite3Files/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(35, 64)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "aa"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(201, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox2.TabIndex = 0
+        Me.TextBox2.Text = "bb"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(358, 64)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox3.TabIndex = 0
+        Me.TextBox3.Text = "cc"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(218, 240)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 40)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(494, 407)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ VBWrite3Files/VBWrite3Files/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub WriteFile(ByVal Dir As String, ByVal FileName As String)
+        Dim FullName As String = Path.Combine(Dir, FileName & ".txt")
+        Try
+            File.WriteAllText(FullName, FileName & "123" & FileName)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dlg As New FolderBrowserDialog
+        If dlg.ShowDialog = DialogResult.OK Then
+            Dim Dir As String = dlg.SelectedPath
+            'MsgBox(Dir)
+            If Not Directory.Exists(Dir) Then
+                Directory.CreateDirectory(Dir)
+            End If
+            WriteFile(Dir, Me.TextBox1.Text.Trim)
+            WriteFile(Dir, Me.TextBox2.Text.Trim)
+            WriteFile(Dir, Me.TextBox3.Text.Trim)
+            MsgBox("ok")
+        End If
+    End Sub
+End Class
